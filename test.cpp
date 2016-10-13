96d8c2f4544437ceb364ccb4b73457903195a498
Marco Porsch <marco.porsch@posteo.de>
2016-10-13 02:08:34 +0200

    add string test

--- test.cpp
+++ test.cpp
@@ -8,4 +8,12 @@ int main(int argc, char **argv) {
     << "-> " << IsAnyEqual(4, 4, 5, 6) << std::endl;
   std::cout << "IsAnyEqual(7, 8, 9, 7) " << std::boolalpha
     << "-> " << IsAnyEqual(7, 8, 9, 7) << std::endl;
+
+  std::cout << "IsAnyEqual(std::string('one'), std::string('two'), std::string('three')) " << std::boolalpha
+    << "-> " << IsAnyEqual(std::string("one"), std::string("two"), std::string("three")) << std::endl;
+  std::cout << "IsAnyEqual(std::string('four'), std::string('four'), std::string('five')) " << std::boolalpha
+    << "-> " << IsAnyEqual(std::string("four"), std::string("four"), std::string("five")) << std::endl;
+  std::cout << "IsAnyEqual(std::string('six'), std::string('seven'), std::string('six')) " << std::boolalpha
+    << "-> " << IsAnyEqual(std::string("six"), std::string("seven"), std::string("six")) << std::endl;
 }
+
