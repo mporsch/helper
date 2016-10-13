170ab8c8db1b21a1f840c730f7053dd161583818
Marco Porsch <marco.porsch@posteo.de>
2016-10-13 02:07:25 +0200

    implement perfect forwarding

--- helper.h
+++ helper.h
@@ -2,14 +2,16 @@
 #define HELPER_H
 
 template<typename T>
-bool IsAnyEqual(T value) {
+bool IsAnyEqual(T&&) {
   return false;
 }
 
 template<typename T, typename... Args>
-bool IsAnyEqual(T value, T first, Args... args) {
-  return (value == first) || IsAnyEqual(value, args...);
+bool IsAnyEqual(T&& value, T&& first, Args&&... args) {
+  return ((value == first) ||
+          (IsAnyEqual(std::forward<T>(value), std::forward<Args>(args)...)));
 }
 
 
 #endif // HELPER_H
+
