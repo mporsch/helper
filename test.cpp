e2f4498016b779d0dc4a341491fb08d61fdcd4e5
Marco Porsch <marco.porsch@posteo.de>
2016-10-13 02:08:20 +0200

    pretty outputs

--- test.cpp
+++ test.cpp
@@ -2,7 +2,10 @@
 #include "helper.h"
 
 int main(int argc, char **argv) {
-  std::cout << "IsAnyEqual(0, 1, 2, 3) " << std::boolalpha << IsAnyEqual(0, 1, 2, 3) << std::endl;
-  std::cout << "IsAnyEqual(4, 4, 5, 6) " << std::boolalpha << IsAnyEqual(4, 4, 5, 6) << std::endl;
-  std::cout << "IsAnyEqual(7, 8, 9, 7) " << std::boolalpha << IsAnyEqual(7, 8, 9, 7) << std::endl;
+  std::cout << "IsAnyEqual(0, 1, 2, 3) " << std::boolalpha
+    << "-> " << IsAnyEqual(0, 1, 2, 3) << std::endl;
+  std::cout << "IsAnyEqual(4, 4, 5, 6) " << std::boolalpha
+    << "-> " << IsAnyEqual(4, 4, 5, 6) << std::endl;
+  std::cout << "IsAnyEqual(7, 8, 9, 7) " << std::boolalpha
+    << "-> " << IsAnyEqual(7, 8, 9, 7) << std::endl;
 }
