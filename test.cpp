8c34654f83a93f914bc3d7e86c2a50a24e263307
Marco Porsch <marco.porsch@posteo.de>
2018-07-28 11:00:52 +0200

    add work queue to test

--- test.cpp
+++ test.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
 #include "helper.h"
+#include "work_queue.h"
+
+#include <iostream>
+#include <thread>
+#include <string>
+
+int count()
+{
+  static int counter = 0;
+  return ++counter;
+}
+
+std::string text(int seconds)
+{
+  return "Sleeping for " + std::to_string(seconds) + " sec";
+}
+
+void sleep(int seconds)
+{
+  std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
 
 int main(int argc, char **argv) {
   std::cout << "IsAnyEqual(0, 1, 2, 3) " << std::boolalpha
@@ -15,5 +35,28 @@ int main(int argc, char **argv) {
     << "-> " << IsAnyEqual(std::string("four"), std::string("four"), std::string("five")) << std::endl;
   std::cout << "IsAnyEqual(std::string('six'), std::string('seven'), std::string('six')) " << std::boolalpha
     << "-> " << IsAnyEqual(std::string("six"), std::string("seven"), std::string("six")) << std::endl;
-}
 
+  WorkQueue workQueue;
+
+  int seconds = workQueue.Assign(count).get();
+  std::cout << workQueue.Assign(text, seconds).get() << std::endl;
+  workQueue.Assign(sleep, seconds).wait();
+
+  seconds = workQueue.Assign(
+    []() -> int
+    {
+      return count();
+    }).get();
+  std::cout << workQueue.Assign(std::bind(
+    [](int seconds) -> std::string
+    {
+      return text(seconds);
+    }, seconds)).get() << std::endl;
+  workQueue.Assign(
+    [](int seconds)
+    {
+      sleep(seconds);
+    }, seconds).wait();
+
+  return EXIT_SUCCESS;
+}
